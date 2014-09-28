386d9e10cc9f35a90806b028ddbccbd66ff3b1f9
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-09-28 19:49:57 -0400

    filters out help and garbage input

--- PA04/grep-lite.c
+++ PA04/grep-lite.c
@@ -8,8 +8,30 @@
 
 int main(int argc, char * * argv)
 {
+  int ind;
   //Check for help
+  for(ind = 1; ind < argc; ind++){
+    if(strcmp(argv[ind], "--help") == 0){
+      printf("Usage: grep-lite [OPTION]... PATTERN\n"
+             "Search for PATTERN in standard input. PATTERN is a\n"
+             "string. grep-lite will search standard input line by\n"
+             "line, and (by default) print out those lines which\n"
+             "contain pattern as a substring.\n\n"
+             "  -v, --invert-match     print non-matching lines\n"
+             "  -n, --line-number      print line numbers with output\n"
+             "  -q, --quiet            suppress all output\n\n"
+             "Exit status is 0 if any line is selected, 1 otherwise;\n"
+             "if any error occurs, then the exit status is 2.");
+      return EXIT_SUCCESS;
+    }
+  }
   //Check middle terms for anything that isn't -v,-n,-q (or print error, return 2)
+  for(ind = 1; ind < (argc - 1); ind++){
+    if((strcmp(argv[ind], "-v") != 0) && (strcmp(argv[ind], "-n") != 0) && (strcmp(argv[ind], "-q") != 0)){
+      fprintf(stderr, "One or more terms not '-v', '-n', or '-q'\n");
+      return 2;
+    }
+  }
   //Check last term for -, if so print error and return 2
   //fgets every line up to 2000 characters
   //strstr every line
