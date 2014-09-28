2efdb6938c22a9ace73b7159bbee56519ed2aeb3
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-09-28 17:09:37 -0400

    cleaned cat, should catch all errors

--- PA04/cat-lite.c
+++ PA04/cat-lite.c
@@ -7,21 +7,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TRUE 1
-#define FALSE 0
-
 int main(int argc, char * * argv)
 {
-  // We want some variables to store which "switches" were set
-  int showHelp = FALSE;
-  int badSwitch = FALSE;
-  int standardIn = FALSE;
-
   FILE *fptr;
 
   // Test for '-', empty, nonexistent file, and help. Otherwise, it's a file
   if(argc == 1)
     //do something with standard input, then exit
+
   else {
     int ind = 1; // we always skip 0, which is the program path
 
@@ -46,27 +39,15 @@ int main(int argc, char * * argv)
       }
     }
 
+    //Everything should be error free, execute code
     for(ind = 1; ind < argc; ind++) {
       if(strcmp(argv[ind], "-") == 0){
         //do something for standard input
       }
-      else if(fopen(argv[ind] == NULL)
-        badSwitch = TRUE;
-      else if(strcmp(argv[ind], "--serve-icecream") == 0)
-        serveIceCream = TRUE;
-      else {
-        badSwitch = TRUE; // best not to run if there's an error
-        fprintf(stderr, "Unknown switch: '%s'\n", argv[ind]);
+      else{
+	//do file stuff
       }
     }
   }
-
-  // And here we do something intelligent:
-  printf("Do you get icecream? ");
-  if(serveIceCream)
-    printf("YES!!!\n");
-  else
-    printf("Sorry, try again next-time.\n");
-
   return EXIT_SUCCESS;
 }
