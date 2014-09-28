8067436fbd9b4e37b144f9171ed8a2574bb235bb
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-09-28 18:52:54 -0400

    commented skeleton for grep

--- PA04/grep-lite.c
+++ PA04/grep-lite.c
@@ -1,4 +1,22 @@
-void main(int argc, char * * argv)
+//Noah Smith
+//Ece264
+//9/26/2014
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char * * argv)
 {
-  printf("%d, %s",argc, argv[0]);
+  //Check for help
+  //Check middle terms for anything that isn't -v,-n,-q (or print error, return 2)
+  //Check last term for -, if so print error and return 2
+  //fgets every line up to 2000 characters
+  //strstr every line
+  //keep track of which lines match, may be inverted
+  //First line number is 1, print number if specified
+  //Print every matching line
+  //If quiet, only return correct exit code
+  //Return 0 if at least one matching line, 1 otherwise
+
 }
