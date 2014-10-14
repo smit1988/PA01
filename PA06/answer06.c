976547e331806a7a93b0eb13ca2caa3fa6a54ce0
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-10-14 18:00:49 -0400

    Completed one step at a time solution

--- PA06/answer06.c
+++ PA06/answer06.c
@@ -10,10 +10,12 @@
 //Deadend, retrace steps to last crossroad
 //Determined there is no whitespace left, head towards the entrance
 
+void mower(char ** maze, int row, int column, int whitespace);
+
 void print_directions(char** maze, int w, int h) {
   //Count the initial whitespace
     int whitespace = 0;
-    int count, count_two, entrance;
+    int count, count_two, entrance,row,column;
     for(count = 0; count < h; count++)
     {
       for(count_two = 0; count_two < w; count_two++)
@@ -27,5 +29,57 @@ void print_directions(char** maze, int w, int h) {
 	    }
 	}
     }
+    //Set mower to entrance
+    row = 0;
+    column = entrance;
+    mower(row, column, whitespace);
+}
 
+void mower(char ** maze, int row, int column, int whitespace){
+  //Set current position to traveled
+  maze[row][column] = TRACK;
+  //Decrease number of whitespace
+  whitespace--;
+  //Don't look past maze
+  //Look for whitespace
+  if(column != 0)
+    {
+      if(maze[row][column - 1] == SPACE)
+	{
+	  //There's a space to the left
+	  printf("W 1\n");
+	  mower(row,column - 1);
+	  printf("E 1\n");
+	}
+    }
+  if(row != h)
+    {
+      if(maze[row + 1][column] == SPACE)
+	{
+	  //There's a space down
+	  printf("S 1\n");
+          mower(row + 1,column);
+          printf("N 1\n");  
+	}
+    }
+  if(column != w)
+    {
+      if(maze[row][column + 1] == SPACE)
+	{
+	  //There's a space to the right
+	  printf("E 1\n");
+          mower(row,column + 1);
+          printf("W 1\n");
+	}
+    }
+  if(row != 0)
+    {
+      if(maze[row - 1][column] == SPACE)
+	{
+	  //There's a space up
+	  printf("N 1\n");
+          mower(row - 1,column);
+          printf("S 1\n");
+	}
+    }
 }
