a7569e431a843ddf411c70c3054848fc17c260bc
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-10-24 19:54:42 -0400

    started the imageload function

--- PA07/answer07.c
+++ PA07/answer07.c
@@ -1,3 +1,7 @@
+//Noah Smith
+//October 24, 2014
+//Ece264
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,9 +9,53 @@
 
 #include "answer07.h"
 
+#define FALSE 0
+#define TRUE 1
+
+static int Header_checkValid(ImageHeader * header)
+{
+  //Make sure that you can read all the bytes of the header
+  //Make sure that the magic_number in the header is correct
+  if(header->magic_number != ECE264_IMAGE_MAGIC_NUMBER) return FALSE;
+  //Make sure that neither the width nor height is zero
+  if(header->width == 0) return FALSE;
+  if(header->height == 0) return FALSE;
+  //Make sure that the comment length is not zero. (Remember, it includes the null-byte.)
+
+}
+
 Image * Image_load(const char * filename)
 {
-  return NULL;
+  FILE * fp = NULL;
+  size_t read;
+  ImageHeader header;
+  Image * tmp_im = NULL, * im = NULL;
+  size_t n_bytes = 0;
+  int err = FALSE;
+
+  if(!err){ //Open filename
+    fp = fopen(filename, "rb");
+    if(!fp){
+      fprintf(stderr, "Failed to open file '%s'\n", filename);
+      err = TRUE;
+    }
+  }
+
+  if(!err){ //Read the header
+    read = fread(&header, sizeof(ImageHeader), 1, fp);
+    if(read != 1){
+      fprintf(stderr, "Failed to read header from '%s'\n", filename);
+      err = TRUE;
+    }
+  }
+
+  if(!err){ //We're only interested in a subset of valid bmp files
+    if(!Header_checkValid(&header)){
+      fprintf(stderr, "Invalid header in '%s'\n", filename);
+      err = TRUE;
+    }
+  }
+
 }
 
 int Image_save(const char * filename, Image * image)
