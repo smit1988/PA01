69c7d7f5b4bfc8e4e972cad383891237fcc5a1af
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-10-25 16:16:31 -0400

    Begin imagesave

--- PA07/answer07.c
+++ PA07/answer07.c
@@ -140,7 +140,64 @@ Image * Image_load(const char * filename)
 
 int Image_save(const char * filename, Image * image)
 {
-  return 0;
+  int err = FALSE;
+  FILE * fp = NULL;
+  uint8_t * buffer = NULL;
+  size_t written = 0;
+
+  //Attempt to open file for writing
+  fp = fopen(filename, "wb");
+  if(fp == NULL){
+    fprintf(stderr, "Failed to open '%s' for writing\n", filename);
+    return FALSE;
+  }
+
+  // Number of bytes stored on each row
+  size_t bytes_per_row = ((24 * image->width + 31)/32) * 4;
+
+  //Prepare the header
+  Imageheader header;
+  header.magic_number = ECE264_IMAGE_MAGIC_NUMBER;
+  header.width = image->width;
+  header.height = image->height;
+  header.comment_len = (strlen(image->comment) + 1);
+
+  if(!err) { //Before writing, we'll need a write buffer
+    buffer = malloc(bytes_per_row);
+    if(buffer == NULL){
+      fprintf(stderr, "Error: failed to allocate write buffer\n");
+      err = TRUE;
+    } else {
+      memset(buffer, 0, bytes_per_row);
+    }
+  }
+
+  if(!err) { //Write pixels
+    uint8_t * read_ptr = image->data;
+    int row, col; //row and column
+    for(row = 0; row < header.height && !err; ++row) {
+      uint8_t * write_ptr = buffer;
+      for(col = 0; col < header.width; ++col) {
+	*write_ptr++ = *read_ptr; // blue
+	*write_ptr++ = *read_ptr; // green
+	*write_ptr++ = *read_ptr; // red
+	read_ptr++; // advance to the next pixel
+      }
+      // Write line to file
+      written = fwrite(buffer, sizeof(uint8_t), bytes_per_row, fp);
+      if(written != bytes_per_row) {
+	fprintf(stderr, "Failed to write pixel data to file\n");
+	err = TRUE;
+      }
+    }
+  }
+
+  //Cleanup
+  free(buffer);
+  if(fp)
+    fclose(fp);
+
+  return !err;
 }
 
 void Image_free(Image * image)
