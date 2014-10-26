bf7b29b35bf5c264d0e71eae3b0cbf6ccaf06c24
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-10-26 16:12:32 -0400

    took care of comment and pixels

--- PA07/answer07.c
+++ PA07/answer07.c
@@ -38,13 +38,28 @@ Image * Image_load(const char * filename){
   img->width = header.width;
   img->height = header.height;
   //Read the comment
-  if(img-> NULL)
-  {
-    return cleanUp(fptr, img);
-  } else {
+  if((img->comment) == NULL)
+    {
+      return cleanUp(fptr, img);
+    } else {
     if(fread(&(img->comment), sizeof(img->comment), 1, fptr) != 1)
       {
 	return cleanUp(fptr, img);
+      }
+  }
+  //Make sure you read the entire comment
+  //Make sure the comment ends in a null-byte
+  if(img->comment[header.comment_len] != '\0')
+    return cleanUp(fptr, img);
+
+  //Read the pixels
+  if((img->data) == NULL)
+    {
+      return cleanUp(fptr, img);
+    } else {
+    if(fread(&(img->data), sizeof(img->data), 1, fptr) != 1)
+      {
+        return cleanUp(fptr, img);
       }
   }
 }
