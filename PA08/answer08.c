79d3d039617ac2ce76004ebfcd03edca0781de80
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-11-01 18:44:49 -0400

    started length

--- PA08/answer08.c
+++ PA08/answer08.c
@@ -41,7 +41,54 @@ int List_length(List * list)
 
 List * List_merge(List * lhs, List * rhs, int (*compar)(const char *, const char*))
 {
-  return 0;
+  //new list
+  List * new = NULL;
+  //keep track of head
+  List * start = new;
+  /*
+  //if at least one list still has elements left
+  while((lhs != NULL) || (rhs != NULL))
+    {
+      //if lhs is greater
+      if(compar(lhs->str,rhs->str) == 1)
+	{
+	  new = lhs;
+	  lhs = lhs->next;
+	  //if rhs is greater or they're equal
+	}else {
+	new = rhs;
+	rhs = rhs->next;
+      }
+      new = new->next;
+      new == NULL;
+      }*/
+  while((lhs != NULL) || (rhs != NULL))
+    {
+      if(rhs == NULL)
+	{
+	  new = lhs;
+	  lhs = lhs->next;
+	}
+      else if(lhs == NULL)
+	{
+	  new = rhs;
+	  rhs = rhs->next;
+	}
+      else
+	{
+	  if(compar(lhs->str,rhs->str) == 1)
+	    {
+	      new = lhs;
+	      lhs = lhs->next;
+	    }
+	  else
+	    {
+	      new = rhs;
+	      rhs = rhs->next;
+	    }
+	}
+      new = new->next;
+    }
 }
 
 List * List_sort(List * list, int (*compar)(const char *, const char*))
