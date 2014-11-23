ace8797754a2c81a61ea9d40f46aba0a68a95de2
Noah Daniel Smith <smit1988@ecegrid-thin1.ecn.purdue.edu>
2014-11-23 10:00:25 -0500

    added name of business to memory

--- PA10/answer10.c
+++ PA10/answer10.c
@@ -14,23 +14,29 @@
 #define BUF 2000
 #define NONE 50000
 
+//Stores the offset of the text and the star rating for a single review
 typedef struct Review_offset_st{
   long int text;
   long int stars;
 }Review_offset;
 
+//Stores all reviews for a given business through an array of Review_offset structures
 typedef struct Review_offset_array_st{
   //malloc size of review_offset * number of entries
   Review_offset * offsets;
 }Review_array;
 
+//Stores the business name, ID, offset for the start of the address, and offset for star rating if there is one
+//In the future maybe change this to store a Review_array
 typedef struct Business_st{
+  char * name;
   int ID;
   long int address;
   long int review;
   struct Business_st * next;
 }Business_struct;
 
+//Binary tree form of Business_struct
 struct YelpDataBST {
   char * name;
   int ID;
@@ -41,15 +47,16 @@ struct YelpDataBST {
 };
 
 
-Business_struct * Create_bus(long int address, long int review, int ID);
+Business_struct * Create_bus(char * name, long int address, long int review, int ID);
 void Destroy_bus(Business_struct * bus);
-int List_length(Business_struct * list);
+//int List_length(Business_struct * list);
 //Business_struct * List_merge(const char* businesses_path, Business_struct * lhs, Business_struct * rhs, int (*compar)(const char *, const char*));
 //Business_struct * List_sort(const char* businesses_path, Business_struct * list, int (*compar)(const char *, const char*));
 
-Business_struct * Create_bus(long int address, long int review, int ID)
+Business_struct * Create_bus(char * name, long int address, long int review, int ID)
 {
   Business_struct * make = NULL;
+  make->name = strdup(name);
   make = malloc(sizeof(Business_struct));
   make->address = address;
   make->review = review;
@@ -63,10 +70,12 @@ void Destroy_bus(Business_struct * bus)
   if(bus != NULL){
     if(bus->next != NULL)
       Destroy_bus(bus->next);
+    free(bus->name);
     free(bus);
   }
 }
 
+/*
 int List_length(Business_struct * list)
 {
   Business_struct * length = list;
@@ -79,7 +88,7 @@ int List_length(Business_struct * list)
     }
   return counter;
 }
-
+*/
 
 struct YelpDataBST* create_business_bst(const char* businesses_path, const char* reviews_path)
 {
@@ -90,6 +99,7 @@ struct YelpDataBST* create_business_bst(const char* businesses_path, const char*
   FILE * Rev_tsv;
   char advance = 'a';
   char * ID_char;
+  char * name;
   int length, count, ID_bus, ID_rev;
   long int address, review;
   Bus_tsv = fopen(businesses_path,"r");
@@ -117,7 +127,14 @@ struct YelpDataBST* create_business_bst(const char* businesses_path, const char*
       ID_bus = atoi(ID_char);
       free(ID_char);
       //ID is now an int, next fgetc will be the first character of the business name
-      //The current position is the start of the address
+      length = 0;
+      name = malloc(sizeof(char) * BUF);
+      do{
+        name[length] = fgetc(Bus_tsv);
+        length++;
+      }while(name[length - 1] != '\t');
+      name[length - 1] = '\0';
+      //Now name is the name of the business in the loop and the the fgetc will be the first character of the address
       address = ftell(Bus_tsv);
       //Find all matching reviews
       //Find review ID
@@ -130,6 +147,11 @@ struct YelpDataBST* create_business_bst(const char* businesses_path, const char*
       ID_char[length - 1] = '\0';
       ID_rev = atoi(ID_char);
       free(ID_char);
+      /*The current code will back up to the start of the line
+if the ID's match, the review long int will be set to the start of the review ID
+if the ID's don't match, the review will be set to an arbitrary value "none"
+"none" shouldn't be a good value but is sketchy code right now
+       */
       fseek(Rev_tsv, (-1 * length), SEEK_CUR);
       if(ID_rev == ID_bus)
 	{
@@ -162,13 +184,16 @@ struct YelpDataBST* create_business_bst(const char* businesses_path, const char*
 	    }
 	}
       fseek(Rev_tsv, (-1 * length), SEEK_CUR);
-      new->next = Create_bus(address, review, ID_bus);
+      new->next = Create_bus(name,address, review, ID_bus);
+      free(name);
       new = new->next;
       printf("%ld %ld\n",new->address,new->review);
       do{
         advance = fgetc(Bus_tsv);
       }while((advance != '\n') && (advance != EOF));
     }
+
+  /*
   //Turn list into binary tree
   static struct YelpDataBST * TreeNode_construct(long int business, long int review, int ID, char * name)
   {
@@ -205,7 +230,7 @@ struct YelpDataBST* create_business_bst(const char* businesses_path, const char*
       }
     return tn;
   }
-
+  */
   /*
   start.next = List_sort(businesses_path,start.next,strcmp);
   new = &start;
